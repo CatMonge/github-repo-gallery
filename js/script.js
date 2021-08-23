22454042f34ef18d1485e323505b8cf417bd3d0f
catarina_m <catarina.monge@gmail.com>
2021-08-22 22:36:55 -0400

    Added user info code to pull up user data

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 const profileInfo = document.querySelector(".overview");
-const username = CatMonge;
+const username = "CatMonge";
 
-const gitUserInfo= async function(){
+const gitUserInfo= async function () {
     const usersInfo= await fetch (`https://api.github.com/users/${username}`);
-    const data= await usersInfo.jason();
+    const data= await usersInfo.json();
+    displayUserInfo(data)
 };
 
+gitUserInfo();
+
+const displayUserInfo = function (data){
+const div = document.createElement("div");
+div.classList.add("user-info");
+div.innerHTML = `<figure>
+<img alt="user avatar" src=${data.avatar_url} />
+</figure>
+<div>
+<p><strong>Name:</strong> ${data.name}</p>
+<p><strong>Bio:</strong> ${data.bio}</p>
+<p><strong>Location:</strong> ${data.location}</p>
+<p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+</div> `;
+
+profileInfo.append(div); 
+};
